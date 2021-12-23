1374b9062c5e268f9d6cdeb3830b1b0e6a114fbc
Kerry Shetline <kerry@shetline.com>
2021-12-22 23:05:34 -0500

    Improved version of queryable promise.

--- src/queryable-promise.ts
+++ src/queryable-promise.ts
@@ -3,45 +3,23 @@ export class QueryablePromise<T> extends Promise<T> {
   private _isResolved = false;
   private _isSettled = false;
 
-  then<TResult1 = T, TResult2 = never>(
-    onResolved?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null,
-    onRejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null
-  ): Promise<TResult1 | TResult2> {
-    const newResolved = onResolved && ((value: T): TResult1 | PromiseLike<TResult1> => {
-      this._isResolved = true;
-      this._isSettled = true;
-      return onResolved(value);
-    });
-    const newRejected = onRejected && ((reason: any): TResult2 | PromiseLike<TResult2> => {
-      this._isRejected = true;
-      this._isSettled = true;
-      return onRejected(reason);
-    });
+  constructor(executor: (resolve: (value: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void) {
+    super((resolve, reject) => executor(
+      value => {
+        resolve(value);
+        this._isResolved = true;
+        this._isSettled = true;
+      },
+      err => {
+        reject(err);
+        this._isRejected = true;
+        this._isSettled = true;
+      }
+    ));
 
-    return super.then(newResolved, newRejected);
-  }
-
-  catch<TResult = never>(
-    onRejected?: ((reason: any) => TResult | PromiseLike<TResult>) | undefined | null
-  ): Promise<T | TResult> {
-    const newRejected = onRejected && ((reason: any): TResult | PromiseLike<TResult> => {
-      this._isRejected = true;
-      this._isSettled = true;
-      return onRejected(reason);
-    });
-
-    return super.catch(newRejected);
-  }
-
-  finally(
-    onFinally?: (() => void) | undefined | null
-  ): Promise<T> {
-    const newFinally = onFinally && ((): void => {
-      this._isSettled = true;
-      return onFinally();
-    });
-
-    return super.finally(newFinally);
+    this._isRejected = false;
+    this._isResolved = false;
+    this._isSettled = false;
   }
 
   get isRejected(): boolean { return this._isRejected; }
