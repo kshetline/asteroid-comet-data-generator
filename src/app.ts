d447895a2ff6582964f51d4c1452d7941d3779a0
Kerry Shetline <kerry@shetline.com>
2021-12-24 19:18:37 -0500

    Set up getBodyData() function.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import { TelnetSequence } from './telnet-sequence';
+import { DateTime } from '@tubular/time';
 
-(async function (): Promise<void> {
+async function getBodyData(name: string, designation: string, isAsteroid: boolean,
+                           startDate: DateTime, endDate: DateTime, interval: string): Promise<void> {
+  const des = designation.startsWith('DES=');
   const ts = new TelnetSequence({
     host: 'horizons.jpl.nasa.gov',
     port: 6775,
@@ -11,29 +14,35 @@ import { TelnetSequence } from './telnet-sequence';
   },
   [
     { prompt: 'Horizons> ', response: 'tty 99999 79' },
-    { prompt: 'Horizons> ', response: '90000033:' },
-    // { prompt: 'Continue [ <cr>=yes, n=no, ? ] : ', response: '' },
+    { prompt: 'Horizons> ', response: designation },
+    des ?
+      { prompt: 'Continue [ <cr>=yes, n=no, ? ] : ', response: '' } :
+      { prompt: null, response: null },
     { prompt: '?,<cr>: ', response: 'E' },
     { prompt: '[o,e,v,?] : ', response: 'e' },
     { prompt: '[ ###, ? ] : ', response: '10' },
     { prompt: '[eclip, frame, body ] : ', response: 'eclip' },
-    { prompt: /00:\d\d] : $/, response: '2021-12-01' },
-    { prompt: /:\d\d] : $/, response: '2021-12-31' },
-    { prompt: '? ] : ', response: '1d' },
+    { prompt: /00:\d\d] : $/, response: startDate.toIsoString(10) },
+    { prompt: /:\d\d] : $/, response: endDate.toIsoString(10) },
+    { prompt: '? ] : ', response: interval },
     { prompt: '?] : ', response: '' },
     { prompt: '[R]edisplay, ? : ', response: 'N' },
     { prompt: 'Horizons> ', response: 'x' }
   ]);
 
+  await ts.process(_line => {}, esc => {
+    if (esc === '\x1B[6n')
+      return '\x1B[24;80R';
+    else if (esc === '\x1B[7m') // This escape sequence (setting reverse video) precedes pausing at a paging prompt
+      return ' ';
+    else
+      return null;
+  });
+}
+
+(async function (): Promise<void> {
   try {
-    await ts.process(_line => {}, esc => {
-      if (esc === '\x1B[6n')
-        return '\x1B[24;80R';
-      else if (esc === '\x1B[7m') // This escape sequence (setting reverse video) precedes pausing at a paging prompt
-        return ' ';
-      else
-        return null;
-    });
+    await getBodyData('Ceres', '1;', true, new DateTime('2021-12-01Z'), new DateTime('2021-12-31Z'), '1d');
   }
   catch (err) {
     console.error(err);
